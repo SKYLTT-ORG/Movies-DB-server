8a077d047ac4b153e374e8a3f45f998ab3f793d3
LalitAswal <Lalit.Aswal143@gmail.com>
2024-10-06 22:02:44 +0530

    redis implementation

--- src/controllers/participantsController.js
+++ src/controllers/participantsController.js
@@ -6,14 +6,36 @@ import {
   deleteParticipantById,
 } from "../services/participantsService.js";
 
+import {
+  storeInCache,
+  fetchFromCache,
+  invalidateCache,
+} from "../lib/redisHelper.js";
+
+const REDIS_KEY = "participants";
+const REDIS_CACHE = 3600;
+
 export const getParticipants = async (req, res) => {
+  const resultFromCache = await fetchFromCache(REDIS_KEY);
+  if (resultFromCache) {
+    res.status(200).json(resultFromCache);
+    return;
+  }
   const result = await getAllParticipants();
+  await storeInCache(REDIS_KEY, result, REDIS_CACHE);
   res.status(200).json(result);
 };
 
 export const getParticipant = async (req, res) => {
   const id = req?.params?.id ?? "";
+  const resultFromCache = await fetchFromCache(REDIS_KEY);
+  if (resultFromCache) {
+    res.status(200).json(resultFromCache);
+    return;
+  }
   const result = await getSingleParticipantById(id);
+  await storeInCache(REDIS_KEY, result, REDIS_CACHE);
+
   if (!result) {
     res.status(404).json({ message: "Participant not found" });
     return;
@@ -34,6 +56,7 @@ export const updateParticipant = async (req, res) => {
   const { name, age, role } = req.body;
 
   const result = await updateParticipantById(id, name, age, role);
+  await invalidateCache(REDIS_KEY);
   if (!result) {
     res.status(404).json({ message: "Participant not found" });
     return;
@@ -44,9 +67,11 @@ export const updateParticipant = async (req, res) => {
 export const deleteParticipant = async (req, res) => {
   const id = req?.params?.id ?? "";
   const result = await deleteParticipantById(id);
+  await invalidateCache(REDIS_KEY);
   if (!result) {
     res.status(404).json({ message: "Participant not found" });
     return;
   }
+
   res.status(204).json();
 };
---
+++ src/lib/redisHelper.js
@@ -0,0 +1,15 @@
+import redisClient from "../config/redis.js";
+
+export const fetchFromCache = async (key) => {
+  const cachedData = await redisClient.get(key);
+  return JSON.parse(cachedData);
+};
+
+export const storeInCache = async (key, data, cacheDuration) => {
+  await redisClient.setex(key, cacheDuration, JSON.stringify(data));
+  return;
+};
+
+export const invalidateCache = async (key) => {
+  await redisClient.del(key);
+};
